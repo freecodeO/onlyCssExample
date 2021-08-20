346104b31853071148122ab9569f3a441efa148c
tongliang.zou <zoutongliang@gmail.com>
2021-08-20 12:30:29 +0800

    add: bankcard 3d

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
 # Only css Example
 
 > 关于CSS一些不错的示例，仅供参考学习。
-> Some good examples about CSS, for reference only.  
+> Some good examples about CSS, for reference only. 
+
+### Run
+- git clone https://github.com/freecodeO/onlyCssExample.git
+- cd onlyCssExample
+- npm install
+- npm run app
+
+server at http://localhost:8080
 
 ### WaterDroplets
-[!alt 实现效果]('')
+[!alt 实现效果]('https://github.com/freecodeO/onlyCssExample/blob/master/example/waterdroplets/water.png')
+
+### BankCard
+[!alt 实现效果]('https://github.com/freecodeO/onlyCssExample/blob/master/example/waterdroplets/bank.png')
--- app.js
+++ app.js
@@ -5,15 +5,21 @@ const koaStatic = require('koa-static');
 
 const app = new koa();
 const router = new koaRouter();
-const staticView = koaStatic(__dirname + '/src/views')
+const staticView = koaStatic(__dirname + '/views')
 
 router.get('/', (ctx) => {
-  ctx.body = 'hello node';
+  ctx.type = 'text/html'
+  ctx.body = fs.createReadStream('./src/home.html');
 })
 
 router.get('/water', (ctx) => {
   ctx.type = 'text/html'
-  ctx.body = fs.createReadStream('./src/views/waterdroplet/index.html');
+  ctx.body = fs.createReadStream('./views/waterdroplet/index.html');
+})
+
+router.get('/bank', (ctx) => {
+  ctx.type = 'text/html'
+  ctx.body = fs.createReadStream('./views/bankcard/index.html');
 })
 
 app.use(staticView);
